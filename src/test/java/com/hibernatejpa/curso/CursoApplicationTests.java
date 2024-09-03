e14075f0d24e6962f35530b04949295731e89e0a
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-03 17:54:07 -0300

    project created

---
+++ src/test/java/com/hibernatejpa/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernatejpa.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
