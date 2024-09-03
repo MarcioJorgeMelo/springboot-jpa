14fd0ee606a5dfd7eaedf882a0bcb7063b0061e8
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-03 20:27:23 -0300

    Service layer,  component  registration

---
+++ src/main/java/com/hibernatejpa/curso/services/UserService.java
@@ -0,0 +1,32 @@
+package com.hibernatejpa.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hibernatejpa.curso.entities.User;
+import com.hibernatejpa.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+
+        return repository.findAll();
+
+    }
+
+    public User findById(Long id) {
+
+        Optional<User>  obj = repository.findById(id);
+
+        return  obj.get();
+
+    }
+
+}
