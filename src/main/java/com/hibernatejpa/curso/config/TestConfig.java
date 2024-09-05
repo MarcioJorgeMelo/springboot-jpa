f2a5e810fa00461fcb220d6c00edf603c67ba18c
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-05 15:01:54 -0300

    Product

--- src/main/java/com/hibernatejpa/curso/config/TestConfig.java
+++ src/main/java/com/hibernatejpa/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.hibernatejpa.curso.entities.Category;
 import com.hibernatejpa.curso.entities.Order;
+import com.hibernatejpa.curso.entities.Product;
 import com.hibernatejpa.curso.entities.User;
 import com.hibernatejpa.curso.entities.enums.OrderStatus;
 import com.hibernatejpa.curso.repositories.CategoryRepository;
 import com.hibernatejpa.curso.repositories.OrderRepository;
+import com.hibernatejpa.curso.repositories.ProductRepository;
 import com.hibernatejpa.curso.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -44,11 +49,19 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers"); 
 
-        userRepository.saveAll(Arrays.asList(u1,u2));
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");              
 
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        userRepository.saveAll(Arrays.asList(u1, u2));
 
-        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4,  p5));
 
     }
 
---
+++ src/main/java/com/hibernatejpa/curso/repositories/JpaProduct.java
@@ -0,0 +1,5 @@
+package com.hibernatejpa.curso.repositories;
+
+public interface JpaProduct<T1, T2> {
+
+}
