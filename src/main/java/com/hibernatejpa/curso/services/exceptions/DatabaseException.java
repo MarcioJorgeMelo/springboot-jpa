4cc86e731ed4ffd4153b9acea3a52ed878d2663c
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-05 19:21:38 -0300

    Add database exception

---
+++ src/main/java/com/hibernatejpa/curso/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.hibernatejpa.curso.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException  (String msg) {
+
+        super(msg);
+
+    }
+
+}
