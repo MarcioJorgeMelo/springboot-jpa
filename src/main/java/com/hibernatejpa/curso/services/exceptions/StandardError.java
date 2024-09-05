ee56757e866f0d7ce474943cab5bb361a7447098
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-05 19:11:41 -0300

    Adds exceptions PART 2

--- src/main/java/com/hibernatejpa/curso/services/exceptions/StandardError.java
+++ src/main/java/com/hibernatejpa/curso/services/exceptions/StandardError.java
@@ -3,8 +3,11 @@ package com.hibernatejpa.curso.services.exceptions;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError implements Serializable {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant timestamp;
 
     private Integer status;
