6868c5c8a365b3354732b8579874a5053e8a73fd
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-05 16:09:41 -0300

    Payment, one-to-one association

--- src/main/java/com/hibernatejpa/curso/config/TestConfig.java
+++ src/main/java/com/hibernatejpa/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.hibernatejpa.curso.entities.Category;
 import com.hibernatejpa.curso.entities.Order;
 import com.hibernatejpa.curso.entities.OrderItem;
+import com.hibernatejpa.curso.entities.Payment;
 import com.hibernatejpa.curso.entities.Product;
 import com.hibernatejpa.curso.entities.User;
 import com.hibernatejpa.curso.entities.enums.OrderStatus;
@@ -83,6 +84,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-07-21T05:42:10Z"), o1);
+
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
     }
 
