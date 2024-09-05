e40d59f000f357aea27f19fdf933b56224d354e2
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-05 18:51:07 -0300

    User update

--- src/main/java/com/hibernatejpa/curso/resources/UserResource.java
+++ src/main/java/com/hibernatejpa/curso/resources/UserResource.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -61,6 +63,14 @@ public class UserResource {
         service.delete(id);
 
         return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
 
     }
 
